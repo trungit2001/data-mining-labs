f5039a78eb937a2f61dd028d28d1da258dafbbe6
trungit2001 <thanhtrungit2001@gmail.com>
2021-10-17 13:47:59 +0700

    create method rto ead and write csv file

--- src/main/java/com/trungit/lab4apriori/AprioriModel.java
+++ src/main/java/com/trungit/lab4apriori/AprioriModel.java
@@ -4,53 +4,140 @@
  */
 package com.trungit.lab4apriori;
 
+import java.io.File;
+import java.io.IOException;
 import weka.associations.Apriori;
 import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.CSVSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.Remove;
 
 /**
  *
  * @author ThanhTrungK15
  */
 public class AprioriModel {
-    private DataSource dtsource;
+    /* Load and save *.arff file */
+    private ArffLoader arffLoader;
+    private ArffSaver arffSaver;
+    
+    /* Load and save *.csv file*/
+    private CSVLoader csvLoader;
+    private CSVSaver csvSaver;
+    
     private Instances dataset;
     private Apriori apriori;
-    private String[] modelConfig, agrsFilterConfig;
     
-    public AprioriModel() {} 
+    public AprioriModel() {
+        this.apriori = new Apriori();
+    } 
+    
     
     public AprioriModel(String pathFile) throws Exception {
-        this.dtsource = new DataSource(pathFile);
-        this.dataset = dtsource.getDataSet();
-        this.apriori = new Apriori();
+        arffLoader = new ArffLoader();
+        arffLoader.setSource(new File(pathFile));
+        dataset = arffLoader.getDataSet();
+        apriori = new Apriori();
     }
 
-    public void convertNumericToNominal(String agrsFilterConfig) throws Exception {
-        this.agrsFilterConfig = weka.core.Utils.splitOptions(agrsFilterConfig);
-        NumericToNominal numericToNominal = new NumericToNominal();
-        
-        numericToNominal.setOptions(this.agrsFilterConfig);
-        numericToNominal.setInputFormat(dataset);
-        
-        /* Áp dụng bộ lọc cho tập dữ liệu và ghi đè bộ dữ liệu cũ */
-        this.dataset =  Filter.useFilter(dataset, numericToNominal);
+    /**
+     * 
+     * Đọc dữ liệu dạng file *.csv lên chương trình
+     * 
+     * @param pathFile Đường dẫn đến file dữ liệu *.csv cần tải lên
+     * @throws Exception 
+     */
+    public void readCSV(String pathFile) throws Exception {
+        csvLoader = new CSVLoader();
+        csvLoader.setSource(new File(pathFile));
+        dataset = csvLoader.getDataSet();
     }
     
-    public void mineAprioriRules(String modelConfig) throws Exception {
-         /* Tạo tham số tinh chỉnh mô hình từ chuỗi tham số truyền vào */
-        this.modelConfig = weka.core.Utils.splitOptions(modelConfig);
+    /**
+     * 
+     * Lưu file dữ liệu dạng *.csv xuống ổ cứng
+     * 
+     * @param pathFile Đường dẫn đến file *.csv cần lưu
+     * @throws IOException 
+     */
+    public void writeCSV(String pathFile) throws IOException {
+        csvSaver = new CSVSaver();
+        csvSaver.setInstances(dataset);
         
-        apriori.setOptions(this.modelConfig);
+        csvSaver.setFile(new File(pathFile));
+        csvSaver.writeBatch();
+    }
+    
+    /**
+     * 
+     * Loại bỏ các thuộc tính dư thừa theo tham số truyền vào
+     * 
+     * @param argsRemoveFilterConfig Chuỗi tham số tinh chỉnh cho bộ lọc Remove
+     * của Weka
+     * @throws Exception 
+     */
+    public void remove(String argsRemoveFilterConfig) throws Exception {
+        String[] argsFilter = weka.core.Utils.splitOptions(argsRemoveFilterConfig);
+        Remove removeFilter = new Remove();
+        
+        removeFilter.setOptions(argsFilter);
+        removeFilter.setInputFormat(dataset);
+        
+        /* Áp dụng bộ lọc và ghi đè lại dữ liệu cũ */
+        dataset = Filter.useFilter(dataset, removeFilter);
+    }
+    
+    
+    /**
+     * 
+     * Chuyển đổi thuộc tính số Numeric sang thuộc tính rời rạc Nominal
+     * 
+     * @param argsNumericToNominalFilterConfig Chuỗi thông số tinh chỉnh cho bộ lọc
+     * NumericToNominal của Weka
+     * @throws java.lang.Exception
+     */
+    public void convertNumericToNominal(String argsNumericToNominalFilterConfig) throws Exception {
+        String[] argsFilter = weka.core.Utils.splitOptions(argsNumericToNominalFilterConfig);
+        NumericToNominal numericToNominalFilter = new NumericToNominal();
+        
+        numericToNominalFilter.setOptions(argsFilter);
+        numericToNominalFilter.setInputFormat(dataset);
+        
+        /* Áp dụng bộ lọc và ghi đè lại dữ liệu cũ */
+        dataset =  Filter.useFilter(dataset, numericToNominalFilter);
+    }
+        
+    /**
+     * 
+     * Khai thác luật kết hợp với thuật toán Apriori
+     * 
+     * @param argsModelConfig Chuỗi thông số tinh chỉnh cho mô hình Apriori
+     * @throws java.lang.Exception
+     */
+    public void mineAprioriRules(String argsModelConfig) throws Exception {
+        String[] modelConfig = weka.core.Utils.splitOptions(argsModelConfig);
+        
+        apriori.setOptions(modelConfig);
         apriori.buildAssociations(dataset);
     }
     
+    /**
+     * 
+     * @return Bộ dữ liệu
+     */
     public String printDataset() {
         return dataset.toString();
     }
     
+    /**
+     * 
+     * @return Các luật kết hợp tìm được với thuật toán Apriori
+     */
     @Override
     public String toString() {
         return apriori.toString();
--- src/main/java/com/trungit/lab4apriori/TestModel.java
+++ src/main/java/com/trungit/lab4apriori/TestModel.java
@@ -16,27 +16,37 @@ public class TestModel {
      */
     public static void main(String[] args) throws Exception {
         /* Đường dẫn đến file dữ liệu cần mở, nhớ đặt 2 dấu "\" */
-        String pathFile = "D:\\Codes\\DataWeka\\apriori.arff";
-//        String pathFile = "D:\\Codes\\DataWeka\\weather.numeric.arff";
+//        String pathFileToLoad = "D:\\Codes\\DataWeka\\arff\\apriori.arff";
+//        String pathFileToLoad = "D:\\Codes\\DataWeka\\arff\\weather.numeric.arff";
         
-        /* Các tham số tinh chỉnh cho mô hình Apriori, FP-Growth và convertNumericToNominal */
+        /* Đọc file *.csv */
+        String pathFileToLoad = "D:\\Codes\\DataWeka\\csv\\apriori.csv";
+
+        /* Lưu vào đường dẫn này */
+        String pathFileToWrite = "D:\\Codes\\DataWeka\\csv\\apriori_preprocessed.csv";
+        
+        /* Các tham số tinh chỉnh cho mô hình Apriori và FP-Growth */
         String aprioriModelConfig = "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1";
-        String fpGrowthModelConfig = "-P 2 -I -1 -N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1";
-        String agrsFilterConfig = "-R first-last";
-                
-        /* Mô hình khai thác dữ liệu theo Apriori rule */
-//        AprioriModel aprioriModel = new AprioriModel(pathFile);
-//        aprioriModel.convertNumericToNominal(agrsFilterConfig);
-//        aprioriModel.mineAprioriRules(aprioriModelConfig);
-//        
-//        System.out.println(aprioriModel.printDataset());
-//        System.out.println(aprioriModel.toString());
+//        String fpGrowthModelConfig = "-P 2 -I -1 -N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1";
         
-        /* Mô hình khai thác dữ liệu theo FP-Growth */
-        FPGrowthModel fpGrowthModel = new FPGrowthModel(pathFile);
-        fpGrowthModel.mineFPGrowthRules(fpGrowthModelConfig);
-        System.out.println(fpGrowthModel.printDataset());
-        System.err.println(fpGrowthModel.toString());
+        /* Các tham số tinh chỉnh cho bộ lọc */
+//        String argsNumericToNominalFilterConfig = "-R first-last";
+        String argsRemoveFilterConfig = "-R 1";
+        /* Mô hình khai thác dữ liệu theo Apriori rule */
+        AprioriModel aprioriModel = new AprioriModel();
+        aprioriModel.readCSV(pathFileToLoad);
+        
+        /* In tập dữ liệu trước khi tiền xử lý */
+        System.out.println(aprioriModel.printDataset());
+        
+        aprioriModel.remove(argsRemoveFilterConfig);
+        
+        /* In tập dữ liệu sau khi tiền xử lý */
+        System.out.println(aprioriModel.printDataset());
+
+        /* Lưu dữ liệu đã tiền xử lý vào file */
+        aprioriModel.writeCSV(pathFileToWrite);
+        System.out.println("File save success!");
     }
     
 }
