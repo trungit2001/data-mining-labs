0ca5e2fc038b1de17212ed5f157f250fa1ee19d3
trungit2001 <thanhtrungit2001@gmail.com>
2021-10-17 12:16:14 +0700

    build success fp-growth on apriori.arff

--- src/main/java/com/trungit/lab4apriori/TestModel.java
+++ src/main/java/com/trungit/lab4apriori/TestModel.java
@@ -16,24 +16,27 @@ public class TestModel {
      */
     public static void main(String[] args) throws Exception {
         /* Đường dẫn đến file dữ liệu cần mở, nhớ đặt 2 dấu "\" */
-//        String pathFile = "D:\\Codes\\DataWeka\\apriori.arff";
-        String pathFile = "D:\\Codes\\DataWeka\\weather.numeric.arff";
+        String pathFile = "D:\\Codes\\DataWeka\\apriori.arff";
+//        String pathFile = "D:\\Codes\\DataWeka\\weather.numeric.arff";
         
-        /* Các tham số tinh chỉnh cho mô hình Apriori và convertNumericToNominal */
-        String modelConfig = "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1";
+        /* Các tham số tinh chỉnh cho mô hình Apriori, FP-Growth và convertNumericToNominal */
+        String aprioriModelConfig = "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1";
+        String fpGrowthModelConfig = "-P 2 -I -1 -N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1";
         String agrsFilterConfig = "-R first-last";
                 
         /* Mô hình khai thác dữ liệu theo Apriori rule */
-        AprioriModel aprioriModel = new AprioriModel(pathFile);
-        aprioriModel.convertNumericToNominal(agrsFilterConfig);
-        aprioriModel.mineAprioriRules(modelConfig);
+//        AprioriModel aprioriModel = new AprioriModel(pathFile);
+//        aprioriModel.convertNumericToNominal(agrsFilterConfig);
+//        aprioriModel.mineAprioriRules(aprioriModelConfig);
+//        
+//        System.out.println(aprioriModel.printDataset());
+//        System.out.println(aprioriModel.toString());
         
-        System.out.println(aprioriModel.printDataset());
-        System.out.println(aprioriModel.toString());
-        
-//        /* Mô hình khai thác dữ liệu theo FP-Growth */
-//        FPGrowthModel fpGrowthModel = new FPGrowthModel(pathFile);
-//        System.out.println(fpGrowthModel.printDataset());
+        /* Mô hình khai thác dữ liệu theo FP-Growth */
+        FPGrowthModel fpGrowthModel = new FPGrowthModel(pathFile);
+        fpGrowthModel.mineFPGrowthRules(fpGrowthModelConfig);
+        System.out.println(fpGrowthModel.printDataset());
+        System.err.println(fpGrowthModel.toString());
     }
     
 }
