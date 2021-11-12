3512798da249bac7d0a5ba348db8e719ca74ac10
trungit2001 <thanhtrungit2001@gmail.com>
2021-11-12 12:27:18 +0700

    refactor lab06 to lab07

--- src/main/java/lab07/DecisionTreeClassifier.java
+++ src/main/java/lab07/DecisionTreeClassifier.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lab06;
+package lab07;
 
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
--- src/main/java/lab07/MainTest.java
+++ src/main/java/lab07/MainTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package lab06;
+package lab07;
 
 import weka.core.Instances;
 
